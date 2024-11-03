d38ff13bd98762057758f44fc0cdf33bbe4fc3d2
RickQuenzer <frederick.quenzer@gmail.com>
2024-11-03 14:58:54 -0700

    number of char used

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 def main():
     book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
